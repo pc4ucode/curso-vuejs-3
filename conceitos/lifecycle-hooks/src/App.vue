d9d6eb0b0e345e1140112c29fe1ee6db7b3d7b26
euler <euler.amorim@hotmail.com>
2021-04-14 23:39:20 -0300

    aula life cycle hooks

---
+++ conceitos/lifecycle-hooks/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Lifecycle hooks</h1>
+</template>
+
+<script>
+import { onMounted } from "vue"
+
+export default {
+  setup() {
+    onMounted(() => {
+      console.log('Foi montado composition API')
+    })
+  }
+}
+// export default {
+//   mounted() {
+//     console.log('foi montado')
+//   },
+// }
+</script>
+
+<style>
+
+</style>
