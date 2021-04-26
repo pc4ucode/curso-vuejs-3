af26047d9d29b2c45091d5df0b94c9e24e922c2c
euler <euler.amorim@hotmail.com>
2021-04-25 23:21:32 -0300

    aula criando tela inicial pt-1 continua

---
+++ dashboard/src/views/Home/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center w-full">
+    <div class="flex flex-col items-center w-4/5 max-w-6xl my-16">
+      <h1 class="text-4xl font-black text-center text-gray-800">
+        Alguma dúvida
+      </h1>
+      <p class="text-lg text-center text-gray-800 font-regular">
+        Quer saber melhor como funciona e quais são os preços?
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
