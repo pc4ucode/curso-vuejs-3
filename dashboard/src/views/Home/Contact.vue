5d51c80f6722fc35f7dac1c9fcece8dbe56e21db
euler <euler.amorim@hotmail.com>
2021-05-18 00:14:57 -0300

    criando tela inicial pt 2 continua

--- dashboard/src/views/Home/Contact.vue
+++ dashboard/src/views/Home/Contact.vue
@@ -7,6 +7,11 @@
       <p class="text-lg text-center text-gray-800 font-regular">
         Quer saber melhor como funciona e quais são os preços?
       </p>
+      <div class="mt-10">
+        <a href="mailto:" class="px-6 py-2 mt-10 font-bold text-white rounded-full bg-brand-main focus:outline-none">
+          Nos mande um e-mail!
+        </a>
+      </div>
     </div>
   </div>
 </template>
---
+++ dashboard/src/views/Home/CustomHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <div class="flex flex-col w-4/5 max-w-6xl">
+      <div>
+        <div class="w-28 lg:w-36">
+          <img class="w-full" src="../../assets/images/logo_white.png" alt="logo">
+        </div>
+        <div class="flex">
+          <button class="px-6 py-2 font-bold rounded-full text-white focus:outline-none">Entrar</button>
+          <button class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outline-none">
+            Crie uma conta
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="postcss" scoped>
+.header {
+  @apply bg-brand-main w-full flex justify-center;
+  height: 700px;
+}
+</style>
